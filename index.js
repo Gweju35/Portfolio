063df34b0c1230be4e652ab098c12c1d6399d09e
Julien Perriguey <julienprgy@gmail.com>
2024-03-28 14:54:43 +0100

    RESPONSIVE

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const observer = new IntersectionObserver(entries => {
     });
 });
 
+// Animations des éléments cachés de la gauche vers la droite
 const hiddenElementsLeft = document.querySelectorAll('.hidden_left');
 hiddenElementsLeft.forEach((el) => observer.observe(el));
 
+// Animations des éléments cachés de la droite vers la gauche
 const hiddenElementsRight = document.querySelectorAll('.hidden_right');
 hiddenElementsRight.forEach((el) => observer.observe(el));
