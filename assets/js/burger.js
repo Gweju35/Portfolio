ab103b5674627529367a745b28bc3a6401ac4c0b
Julien Perriguey <julienprgy@gmail.com>
2024-05-09 18:16:02 +0200

    Probleme d'overlay sur menu Fixed

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -3,15 +3,18 @@ var menu = document.querySelector(".header-wrapper .menu");
 var overlay = document.querySelector(".overlay");
 
 /* Si la classe active est déjà présente, on l'enlève, sinon on lui donne */
-document.querySelectorAll(".header-burger, .menu-item").forEach(function(element) {
+document.querySelectorAll(".header-burger").forEach(function(element) {
     element.addEventListener("click", function() {
         menuBurger.classList.toggle("active");
         menu.classList.toggle("active");
+        overlay.classList.toggle("active");
     });
 });
 
-document.querySelectorAll(".header-burger").forEach(function(element) {
+document.querySelectorAll(".menu-item").forEach(function(element) {
     element.addEventListener("click", function() {
-        overlay.classList.toggle("active");
+        menuBurger.classList.remove("active");
+        menu.classList.remove("active");
+        overlay.classList.remove("active");
     });
 });
