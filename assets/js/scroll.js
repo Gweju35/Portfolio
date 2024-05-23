d3ec8446c59bdc7a17089c93fcbee0346b5dd4e4
Julien Perriguey <julienprgy@gmail.com>
2024-05-23 17:08:58 +0200

    meme commit que le précédent, mais pour le logo du site

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,11 +1,11 @@
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     var header = document.querySelector('.header-section');
     var scrollPosition = window.scrollY;
 
     // Si un scroll est détecté on ajoute la classe active à la balise header
     if (scrollPosition > 0) {
         header.classList.add('active');
-    } 
+    }
     // Si on remonte tout en haut de la page on retire la classe active
     else {
         header.classList.remove('active');
@@ -13,16 +13,26 @@ window.addEventListener('scroll', function() {
 });
 
 // Permet de ne pas modifier l'URL lorsque l'on clique sur un lien de la navbar
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var links = document.querySelectorAll('.navbar-links');
-    links.forEach(function(link) {
-      link.addEventListener('click', function(e) {
-        e.preventDefault(); 
-        var targetId = this.getAttribute('href'); 
-        var targetElement = document.querySelector(targetId); 
-        if (targetElement) {
-          targetElement.scrollIntoView({ behavior: 'smooth' });
-        }
-      });
+    links.forEach(function (link) {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            var targetId = this.getAttribute('href');
+            var targetElement = document.querySelector(targetId);
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
     });
-  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var goTopLink = document.querySelector('.goTop');
+    if (goTopLink) {
+        goTopLink.addEventListener('click', function (e) {
+            e.preventDefault();
+            window.scrollTo({ top: 0, behavior: 'smooth' });    
+        });
+    }
+});
