e59ba9de58606ed6229dd36f7e563915a54ebb0c
Julien Perriguey <julienprgy@gmail.com>
2025-05-03 10:43:11 +0200

    Button

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="titleSection">
+    <h2 class="title">
+      <span class="shape"><span>{{ title }}</span></span>
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
